710373e0b7cedff82797e0239bc249aca4c4ba72
Nishanth Chirala <nishanthchirala5@gmail.com>
2026-09-09 10:35:36 +0530

    stripe

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -20,6 +20,34 @@ export const stripeWebhooks = async (req, res) => {
 
   try {
     switch (event.type) {
+      case 'checkout.session.completed': {
+        const session = event.data.object;
+        const { bookingId } = session.metadata || {};
+
+        if (session.payment_status !== 'paid' || !bookingId) {
+          break;
+        }
+
+        const booking = await Booking.findOneAndUpdate(
+          { _id: bookingId, isPaid: false },
+          {
+            isPaid: true,
+            paymentLink: '',
+          },
+          { new: true }
+        );
+
+        if (!booking) {
+          break;
+        }
+
+        await inngest.send({
+          name: 'app/show.booked',
+          data: { bookingId },
+        });
+
+        break;
+      }
       case 'payment_intent.succeeded': {
         const paymentIntent = event.data.object;
         const sessionList = await stripeInstance.checkout.sessions.list({
@@ -27,12 +55,24 @@ export const stripeWebhooks = async (req, res) => {
         });
 
         const session = sessionList.data[0];
-        const { bookingId } = session.metadata;
+        const { bookingId } = session?.metadata || {};
 
-        await Booking.findByIdAndUpdate(bookingId, {
-          isPaid: true,
-          paymentLink: '',
-        });
+        if (!bookingId) {
+          break;
+        }
+
+        const booking = await Booking.findOneAndUpdate(
+          { _id: bookingId, isPaid: false },
+          {
+            isPaid: true,
+            paymentLink: '',
+          },
+          { new: true }
+        );
+
+        if (!booking) {
+          break;
+        }
 
         await inngest.send({
           name: 'app/show.booked',
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 await connectDB();
 
